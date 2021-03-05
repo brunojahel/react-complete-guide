927d8671b655d3f490f7e4b84e7e6b79e5726dd3
Jahel <brunojahel@gmail.com>
2021-03-05 16:11:54 -0300

    lecture 3.49 - styling (inline and with css files)

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
 
   state = {
     persons: [
-      { name: 'Max', age:'20'},
+      { name: 'Maximillian Pegasus', age:'20'},
       { name: 'Bruno', age:'25'},
       { name: 'Harry', age:'30'},
     ]
@@ -15,25 +15,47 @@ class App extends Component {
   switchNameHandler = (newName) => {
     this.setState({
       persons: [
-        { name: newName, age:'25'},
+        { name: newName, age:'20'},
         { name: 'Bruno', age:'25'},
         { name: 'Harry', age:'30'},
       ]
     })
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Maximillian Pegasus', age:'25'},
+        { name: event.target.value, age:'25'},
+        { name: 'Harry', age:'30'},
+      ]
+    })
+  }
+
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
-        <button onClick={() => this.switchNameHandler('Maximillian Pegasus')}>Switch name</button>
+        <button
+            style={style}
+            onClick={() => this.switchNameHandler('Maximillian Pegasus')}>Switch name</button>
         <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age} />
         <Person
             name={this.state.persons[1].name}
             age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'King of monster duel')}>And I like to swim</Person>
+            click={this.switchNameHandler.bind(this, 'King of monster duel')}
+            changed={this.nameChangedHandler}>And I like to swim</Person>
         <Person
             name={this.state.persons[2].name}
             age={this.state.persons[2].age}/>
