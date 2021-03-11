32a88c2af2466bb129522ed18ae158b4e05db82c
Jahel <brunojahel@gmail.com>
2021-03-11 15:18:07 -0300

    lecture 4.63 - changing items of the persons array more efficiently

--- src/App.js
+++ src/App.js
@@ -6,27 +6,32 @@ class App extends Component {
 
   state = {
     persons: [
-      {name: 'Maximillian Pegasus', age: '20'},
-      {name: 'Bruno', age: '25'},
-      {name: 'Harry', age: '30'},
+      {id:'1', name: 'Maximillian Pegasus', age: '20'},
+      {id:'2', name: 'Bruno', age: '25'},
+      {id:'3', name: 'Harry', age: '30'},
     ],
     otherState: 'outro valor',
     showPersons: false
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
     this.setState({persons: persons});
   }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        {name: 'Maximillian Pegasus', age: '25'},
-        {name: event.target.value, age: '25'},
-        {name: 'Harry', age: '30'},
-      ]
+      persons: persons
     })
   }
 
@@ -48,23 +53,28 @@ class App extends Component {
 
     if (this.state.showPersons) {
       persons = (
-          <div>
-            {this.state.persons.map((person, index) => {
-              return <Person name={person.name} age={person.age} click={() => this.deletePersonHandler(index)}/>
-            })}
-          </div>
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              name={person.name}
+              age={person.age}
+              click={() => this.deletePersonHandler(index)}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
+          })}
+        </div>
       );
     }
 
     return (
-        <div className="App">
-          <h1>Hi, I am a React App</h1>
-          <button
-              style={style}
-              onClick={this.togglePersonsHandler}>Toggle persons
-          </button>
-          {persons}
-        </div>
+      <div className="App">
+        <h1>Hi, I am a React App</h1>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle persons
+        </button>
+        {persons}
+      </div>
     );
   }
 }
